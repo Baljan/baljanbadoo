43f9692f6b3ea4be8e61ae4df3afc2da74926893
Emil <emil19@users.noreply.github.com>
2022-03-07 16:05:23 +0100

    fix button animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import {
   AnimatePresence,
   motion,
   useMotionValue,
-  useTransform,
+  useTransform
 } from "framer-motion";
-import { FaTimes, FaHeart, FaStar } from "react-icons/fa";
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import { FaHeart, FaStar, FaTimes } from "react-icons/fa";
 import styles from "../styles/Home.module.css";
-import { useState } from "react";
 
 type SwipeType = "like" | "nope" | "superlike";
 
@@ -17,6 +16,7 @@ type Target = {
   id: string;
   image: string;
   name: string;
+  iter?: number;
 };
 
 const targets: Target[] = [
@@ -35,6 +35,36 @@ function getRandomElement<T>(items: T[]): T {
   return items[Math.floor(Math.random() * items.length)];
 }
 
+function shuffle<T>(array: T[]) {
+  let currentIndex = array.length;
+  let randomIndex: number;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
+const createTargetGenerator = () => {
+  let cnt = 1;
+  return () => {
+    cnt++;
+    return shuffle(targets.map((t) => ({ ...t, iter: cnt })));
+  };
+};
+const generateTargets = createTargetGenerator();
+
+
 const Home: NextPage = () => {
   const x = useMotionValue(0);
   const y = useMotionValue(0);
@@ -44,34 +74,42 @@ const Home: NextPage = () => {
   const nope = useTransform(x, [-250, -50], [1, 0]);
 
   // Get random element from a list
-  const [target, setTarget] = useState<Target>(getRandomElement(targets));
-  const [counter, setCounter] = useState(0);
-  const [exitDirection, setExitDirection] = useState<SwipeType>("like");
+  const [queue, setQueue] = useState<Target[]>([]);
+  // const [showing, setShowing] = useState<boolean>(true);
+  const [exitDirection, setExitDirection] = useState<SwipeType>("superlike");
+
+  useEffect(() => {
+    if (!queue.length) {
+      setQueue(generateTargets());
+    }
+  }, [queue]);
 
   const submitSwipe = (swipeType: SwipeType) => {
     setExitDirection(swipeType);
-    setTarget(getRandomElement(targets));
-    setCounter((i) => i + 1);
+    setQueue((q) => q.slice(1));
   };
 
   const variants = {
     initial: {
       scale: 0,
       opacity: 0,
-      x: 0,
-      y: 0,
+      translateX: 0,
+      translateY: 0,
     },
     main: {
       scale: 1,
       opacity: 1,
-      x: 0,
-      y: 0,
+      translateX: 0,
+      translateY: 0,
     },
     exit: (d: SwipeType) => ({
       scale: 0.5,
       opacity: 1,
-      x: d == "like" ? "150%" : d == "nope" ? "-150%" : 0,
-      y: d == "superlike" ? "-100%" : 0,
+      translateX: d == "like" ? "150%" : d == "nope" ? "-150%" : 0,
+      translateY: d == "superlike" ? "-100%" : 0,
+      transition: {
+        duration: 0.3
+      }
     }),
   };
 
@@ -86,63 +124,64 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <AnimatePresence exitBeforeEnter>
-          <motion.div
-            key={counter}
-            variants={variants}
-            custom={exitDirection}
-            initial="initial"
-            animate="main"
-            exit="exit"
-            className={styles.swipeCard}
-            drag
-            dragElastic={0.2}
-            dragSnapToOrigin
-            onDragEnd={(e, info) => {
-              const {
-                offset: { x, y },
-              } = info;
-              let swipeType: SwipeType | undefined;
-              if (x > 200) {
-                swipeType = "like";
-              } else if (x < -200) {
-                swipeType = "nope";
-              } else if (y < -200) {
-                swipeType = "superlike";
-              }
-              if (swipeType) {
-                submitSwipe(swipeType);
-              }
-            }}
-            style={{
-              x,
-              y,
-              rotate: rotation,
-            }}
-          >
-            <div className={styles.swipeCardImage}>
-              <img src={target.image} />
-            </div>
-            <h2>{target.name}</h2>
+        <AnimatePresence exitBeforeEnter custom={exitDirection}>
+          {queue.length ? (
             <motion.div
-              className={`${styles.swipeHint} ${styles.swipeHintLike}`}
-              style={{ opacity: like }}
+              key={`${queue[0].id}-${queue[0].iter}`}
+              variants={variants}
+              initial="initial"
+              animate="main"
+              exit="exit"
+              className={styles.swipeCard}
+              drag
+              dragElastic={0.2}
+              dragSnapToOrigin
+              onDragEnd={(e, info) => {
+                const {
+                  offset: { x, y },
+                } = info;
+                let swipeType: SwipeType | undefined;
+                if (x > 200) {
+                  swipeType = "like";
+                } else if (x < -200) {
+                  swipeType = "nope";
+                } else if (y < -200) {
+                  swipeType = "superlike";
+                }
+                if (swipeType) {
+                  submitSwipe(swipeType);
+                }
+              }}
+              style={{
+                x,
+                y,
+                rotate: rotation,
+              }}
             >
-              Like
+              <div className={styles.swipeCardImage}>
+                <img src={queue[0].image} />
+              </div>
+              <h2>{queue[0].name}</h2>
+              <motion.div
+                className={`${styles.swipeHint} ${styles.swipeHintLike}`}
+                style={{ opacity: like }}
+              >
+                Like
+              </motion.div>
+              <motion.div
+                className={`${styles.swipeHint} ${styles.swipeHintNope}`}
+                style={{ opacity: nope }}
+              >
+                Nope
+              </motion.div>
+              <motion.div
+                className={`${styles.swipeHint} ${styles.swipeHintSuperlike}`}
+                style={{ opacity: superlike }}
+              >
+                Superlike
+              </motion.div>
             </motion.div>
-            <motion.div
-              className={`${styles.swipeHint} ${styles.swipeHintNope}`}
-              style={{ opacity: nope }}
-            >
-              Nope
-            </motion.div>
-            <motion.div
-              className={`${styles.swipeHint} ${styles.swipeHintSuperlike}`}
-              style={{ opacity: superlike }}
-            >
-              Superlike
-            </motion.div>
-          </motion.div>
+          ) : null}
         </AnimatePresence>
         <div className={styles.buttonRow}>
           <button

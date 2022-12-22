11fc4f520721256b8e429f514d34aa142c66d076
Emil <emil19@users.noreply.github.com>
2022-12-22 20:44:56 +0100

    Redo deployment to gh actions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
-const prefix = isProd ? "/baljanbadoo" : "";
+const prefix = isProd ? "" : "";
 
 const nextConfig = {
   reactStrictMode: true,
